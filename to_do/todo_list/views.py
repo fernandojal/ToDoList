1bd29a64b1d3437ac9bec8d943f58b920c81ace6
Fernando <fernandojal.une@gmail.com>
2021-06-26 14:42:06 -0400

    modified http response when field is empty

--- to_do/todo_list/views.py
+++ to_do/todo_list/views.py
@@ -7,16 +7,13 @@ from django.contrib import messages
 def home(request):  
     if request.method == 'POST':
         item = request.POST.get('Item')
-        list = ListForm({'item': item})
-        print(list.is_valid())
-        if list.is_valid():
-            list.save()
-            all_items = List.objects.all()
-            messages.success(request,('Item Has Been Added To List!'))
-            return render(request, 'home.html', {'all_items': all_items})        
-    else:
-        all_items = List.objects.all()
-        return render(request, 'home.html', {'all_items': all_items })
+        if item is not None:
+            list = ListForm({'item': item})
+            if list.is_valid():
+                list.save()
+                messages.success(request,('Item Has Been Added To List!'))
+    all_items = List.objects.all()        
+    return render(request, 'home.html', {'all_items': all_items})        
 
 def delete(request, list_id):
     item = List.objects.get(pk=list_id)
@@ -24,13 +21,13 @@ def delete(request, list_id):
     messages.success(request, ('Item Has Been Removed!'))
     return redirect('home')
 
-def cross_off(request, list_id):
+def select(request, list_id):
     item = List.objects.get(pk=list_id)
     item.completed = True
     item.save()
     return redirect('home')
 
-def uncross(request, list_id):
+def unselect(request, list_id):
     item = List.objects.get(pk=list_id)
     item.completed = False
     item.save()
